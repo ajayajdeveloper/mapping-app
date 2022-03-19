8c92de4dfd8bd57316541a76f942f0ebbebe97b8
Ajaydev <ajaykumaraj2529@gmail.com>
2022-03-19 19:20:40 +0530

    my changes

--- index.js
+++ index.js
@@ -21,16 +21,23 @@ mongoose.connect(process.env.Mongo_URL,{useNewUrlParser:true})
 app.use("/api/users",userRoute)
 app.use("/api/pins",pinRoute)
 
-if(process.env.NODE_ENV == 'production')
-{
-    app.use('/' , express.static('client/build'))
 
-    app.get("*", (req, res) => {
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
+// if(process.env.NODE_ENV == 'production')
+// {
+//     app.use('/' , express.static('client/build'))
+
+//     app.get("*", (req, res) => {
+
+//         res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
       
-    });
-}
+//     });
+// }
 
 app.listen(process.env.PORT || 5000 ,() =>
 {
